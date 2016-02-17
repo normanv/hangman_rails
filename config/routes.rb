7497b368db7edbb2cdf7b0969774e416b7f8f32b
Norman Vourlat <norman@Normans-MacBook-Pro.local>
2016-02-17 16:48:28 +1300

    tutorial based on how to create a blog with rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  Rails.application.routes.draw do
+
+  resources :games
+
+  root 'hangman#index'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
